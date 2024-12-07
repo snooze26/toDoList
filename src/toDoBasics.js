e6eaf57ccde4cc1daeb8905e55db1cd07903786c
snooze26 <langdon.douglas26@gmail.com>
2024-12-06 19:42:51 -0600

    add newFile for todo Basics

---
+++ src/toDoBasics.js
@@ -0,0 +1,27 @@
+
+export class toDo {
+    constructor(title, description, dueDate, priority, notes){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes; 
+    }
+
+    displayInfo(){
+        console.log(`${this.title} | ${this.description} | due by ${this.dueDate} | the priority level is ${this.priority}`);
+
+    }
+}
+
+export function retrieveTodo() {
+    const title = prompt("Enter the title:");
+    const description = prompt("Enter the description");
+    const dueDate = prompt("When is this due?");
+    const priority = prompt("Rate the level of priority: High, Medium, Low");
+    const notes = prompt("Insert any notes");
+
+    return new toDo(title, description, dueDate, priority, notes);
+}
+const usersToDo = retrieveTodo();
+usersToDo.displayInfo();
